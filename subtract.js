1a1e45aa725e13e3ec974ed2cdd2def07b900f7e
Kapildev.Koli <Kapildev.Koli@cytel.com>
2019-01-27 19:59:01 +0530

    add.js file info updated from local repo

--- subtract.js
+++ subtract.js
@@ -1,6 +1,10 @@
 // subtract.js
-
 // return subtraction of two numbers
-function subtract(a, b) {
-	return a - b;
+function subtract(a, b, bMINUSa) {
+    if (bMINUSa === true) {
+        return b - a;
+    }
+    else {
+        return a - b;
+    }
 };
